5f0e57fb4eca89440f237be5ee5ae6bb501e44e3
Jeric Frank Sasil <jericfrank@gmail.com>
2017-05-01 14:15:58 +0800

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+	return [
+		{ 'title' : 'Fuck yes' },
+		{ 'title' : 'Haha' },
+		{ 'title' : 'I miss shit' },
+		{ 'title' : 'yaaamn!' },
+		{ 'title' : 'naaah!' },
+		{ 'title' : 'Fuck no' }
+	]
+}
